bd1fbcb0c7436564adef5d8c17edcb9dade63816
Simon Weyand <simon.weyand@student.uni-siegen.de>
2023-06-04 18:14:19 +0200

    Add temp Java file

---
+++ Java/src/temp/Main.java
@@ -0,0 +1,7 @@
+package temp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("We will replace this temporary folder with the tasks as soon as they are available!");
+    }
+}
